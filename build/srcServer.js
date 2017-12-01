c4323cc46cb9caa7d043543956d1a0ffbe45ef84
srsandy <sandeepranjan2007@yahoo.co.in>
2017-12-02 02:08:36 +0530

    webpack added

--- build/srcServer.js
+++ build/srcServer.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const path = require('path');
-const open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../wepack.config.dev'
 
 const PORT = 3000 || process.env.port;
 const app = express();
+const complier = webpack(config);
 
-app.get('/', function(req, res){
+app.use(require('webpack-dev-middleware')(complier, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.get('/', (req, res) =>{
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(PORT, function(err){
+app.listen(PORT, err =>{
   if(err) throw err;
   open('http://localhost:' + PORT);
 });
--- build/startMessage.js
+++ build/startMessage.js
@@ -1,4 +1,5 @@
-const chalk = require('chalk');
+import chalk from 'chalk';
+//const chalk = require('chalk');
 //npm module that allows to specify the color of the terminal statement
 
 console.log(chalk.green('Started app in dev mode'));
